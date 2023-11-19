36cc8dac6be61c2a2463a07e0faf79649fe8859a
EdelCorbett <131147963+EdelCorbett@users.noreply.github.com>
2023-11-19 19:04:50 +0000

    Move procfile

--- poolhall/settings.py
+++ poolhall/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['pp4poolhall.herokuapp.com', 'localhost','127.0.0.1']
+ALLOWED_HOSTS = ['pp4poolhall.herokuapp.com', 'localhost']
 
 
 # Application definition
