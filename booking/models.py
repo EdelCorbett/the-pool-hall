8b2359085844ade8f8b348bca86d9c08100f41ee
EdelCorbett <131147963+EdelCorbett@users.noreply.github.com>
2023-11-23 13:30:25 +0000

    Models created and migrated

---
+++ booking/models.py
@@ -0,0 +1,87 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
+
+"""
+Model for table number and if game size is singles or doubles
+"""
+class Table(models.Model):
+    size_choices = (
+        ('1', '1, singles'),
+        ('2', '2, doubles'),
+        )
+    table_number = models.IntegerField(unique=True)
+    table_size = models.CharField(choices = size_choices, default='1', max_length=2)
+
+    def __str__(self):
+        return f"Table {self.table_number}"
+
+
+class CustomUser(AbstractUser):
+    full_name = models.CharField(max_length=25)
+    email = models.EmailField(unique=True,blank=True, null=True)
+    membership_id = models.CharField(max_length=10, unique=True)
+    membership_start_date = models.DateField(null=True, blank=True)
+    membership_end_date = models.DateField(null=True, blank=True)
+
+
+class TimeSlots(models.Model):
+    DAY_CHOICES = [
+        ('Monday', 'Monday'),
+        ('Tuesday', 'Tuesday'),
+        ('Wednesday', 'Wednesday'),
+        ('Thursday', 'Thursday'),
+        ('Friday', 'Friday'),
+        ('Saturday', 'Saturday'),
+        ('Sunday', 'Sunday')
+]
+
+    TIME_CHOICES = [
+        ('15:00:00', '15:00:00'),
+        ('16:00:00', '16:00:00'),
+        ('17:00:00', '17:00:00'),
+        ('18:00:00', '18:00:00'),
+        ('19:00:00', '19:00:00'),
+        ('20:00:00', '20:00:00'),
+        ('21:00:00', '21:00:00'),
+        ('22:00:00', '22:00:00'),
+    ]
+    
+    day = models.CharField(choices=DAY_CHOICES, default='Monday',max_length=9)
+    start_time =models.TimeField(choices=TIME_CHOICES, default='15:00:00')
+    is_open = models.BooleanField(default=True)
+    table = models.ForeignKey(Table, on_delete=models.CASCADE,related_name='timeslots')
+
+    class Meta:
+        unique_together = ('day', 'start_time', 'table')
+        
+
+    def __str__(self):
+        return f"{self.get_day_display()} at {self.start_time.strftime('%H:%M')}"
+    
+    def get_all_tables(self):
+        return self.table.all()
+    
+    def get_available_tables(self):
+        return self.table.filter(is_open=True)
+    
+    def get_booked_tables(self):
+        return self.table.filter(is_open=False)
+    
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            if TimeSlots.objects.filter(day=self.day,start_time=self.start_time,table=self.table).exists():
+                raise ValidationError('Time slot already exists for this table')
+            else:
+                super().save(*args, **kwargs)
+        else:
+            super().save(*args, **kwargs)      
+
+
+class Bookings(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE,related_name='bookings')
+    booking_time = models.TimeField()
+    booking_date = models.DateField()
+    is_cancelled = models.BooleanField(default=False)
+    is_edited = models.BooleanField(default=False)
+    table = models.ForeignKey(Table, on_delete=models.SET_NULL, null=True,blank=True)
