cd4d729bba7dda74111bb8e5b2cb4196cc42fcd0
EdelCorbett <131147963+EdelCorbett@users.noreply.github.com>
2023-12-02 12:55:28 +0000

    Register Modelsin admin.py

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import CustomUser, Table, Bookings, TimeSlots
 # Register your models here.
+admin.site.register(CustomUser)
+admin.site.register(Table)
+admin.site.register(Bookings)
+admin.site.register(TimeSlots)
