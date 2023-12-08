0de92362a8b9b01f3fbed5645b85469e8ea46b2d
EdelCorbett <131147963+EdelCorbett@users.noreply.github.com>
2023-12-08 01:59:28 +0000

    Add timeslotform

--- booking/forms.py
+++ booking/forms.py
@@ -23,8 +23,11 @@ class MemberForm(UserCreationForm):
 
             if not user.membership_id:
                 while True:
-                    new_membership_id = ''.join(secrets.choice(string.ascii_uppercase + string.digits) for _ in range(10))
-                    if not CustomUser.objects.filter(membership_id=new_membership_id).exists():
+                    new_membership_id = ''.join(secrets.choice(
+                        string.ascii_uppercase + string.digits) 
+                        for _ in range(10))
+                    if not CustomUser.objects.filter(
+                        membership_id=new_membership_id).exists():
                         user.membership_id = new_membership_id
                         break
 
@@ -32,6 +35,9 @@ class MemberForm(UserCreationForm):
                 user.save()
 
             return user
+    
+class TimeSlotForm(forms.Form):
+    timeslots = forms.ChoiceField(choices=[(f'{hour:02}:{minute:02}', f'{hour:02}:{minute:02}') for hour in range(15, 22) for minute in range(0, 60, 15)])
 
     
     
@@ -40,22 +46,28 @@ class BookingForm(forms.ModelForm):
         model = Bookings
         fields = ['user', 'booking_date','booking_time','table']
         widgets = {
-            'user': forms.TextInput(attrs={'class': 'form-control'}),
+            'user': forms.HiddenInput(),
             'booking_date': forms.DateInput(attrs={'type': 'date'}),
-            'booking_time': forms.TimeInput(attrs={'type': 'time'}),
+            'booking_time': forms.TimeInput(attrs={'type': 'time','format': '%H:%M'}),
             'table': forms.Select(attrs={'class': 'form-control'}),
+            
         
         }
     def __init__(self, *args, **kwargs):
+        user = kwargs.pop('user', None)
         super(BookingForm, self).__init__(*args, **kwargs)
+        self.fields['user'].initial = user
+
         self.helper = FormHelper()
         self.helper.layout = Layout(
             'user',
             'booking_date',
             'booking_time',
             'table',
+
             FormActions(
                 Submit('submit', 'Submit', css_class='btn btn-primary')
             )
         )
 
+    
