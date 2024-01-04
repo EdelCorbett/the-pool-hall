02b9bd9caa8441be64a7180f5822fec94744e948
EdelCorbett <131147963+EdelCorbett@users.noreply.github.com>
2024-01-04 11:04:10 +0000

    Add blog events pages

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from booking.models import CustomUser
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+STATUS = (
+    (0, "Draft"),
+    (1, "Publish")
+)
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="authored_posts")
+    content = models.TextField()
+    status = models.IntegerField(choices=STATUS, default=0)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    featured_image = CloudinaryField('image', default='placeholder')
+    likes = models.ManyToManyField(CustomUser, related_name='liked_posts')
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+    
+    def total_likes(self):
+        return self.likes.count()
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+    related_name="comments")
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
+
+
