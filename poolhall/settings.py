2d80256c49b4d2e362a355e4bff1f09855c42c53
EdelCorbett <131147963+EdelCorbett@users.noreply.github.com>
2024-01-12 14:38:14 +0000

    fixing settings for deployment

--- poolhall/settings.py
+++ poolhall/settings.py
@@ -13,8 +13,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR,'templates')
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-DEBUG = True
-
+DEBUG = False
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['pp4poolhall-b3360ca06d73.herokuapp.com',
@@ -33,15 +32,14 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    
-    'django.contrib.staticfiles',
     'cloudinary_storage',
+    'django.contrib.staticfiles',
     'cloudinary',
     'crispy_forms',
     'django_summernote',
     'booking',
     'blog',
-]
+    ]
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 SITE_ID = 1
 
@@ -132,7 +130,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
