712de641cc9d290d2d8eb9086cb5a826f1d54436
EdelCorbett <131147963+EdelCorbett@users.noreply.github.com>
2024-01-13 14:49:24 +0000

    Add to Readme and tesing

--- poolhall/settings.py
+++ poolhall/settings.py
@@ -13,7 +13,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR,'templates')
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-DEBUG = False
+DEBUG = True
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['pp4poolhall-b3360ca06d73.herokuapp.com',
